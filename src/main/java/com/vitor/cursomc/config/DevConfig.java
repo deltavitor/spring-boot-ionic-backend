3209a4cf90271684952a9d27ba5abb5b0bf72d31
deltavitor <cvitor1618@gmail.com>
2020-09-24 23:43:46 -0300

    Email HTML

--- src/main/java/com/vitor/cursomc/config/DevConfig.java
+++ src/main/java/com/vitor/cursomc/config/DevConfig.java
@@ -1,14 +1,14 @@
 package com.vitor.cursomc.config;
 
 import java.text.ParseException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
 import com.vitor.cursomc.services.DBService;
+import com.vitor.cursomc.services.EmailService;
+import com.vitor.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +29,10 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
--- src/main/java/com/vitor/cursomc/config/TestConfig.java
+++ src/main/java/com/vitor/cursomc/config/TestConfig.java
@@ -1,13 +1,14 @@
 package com.vitor.cursomc.config;
 
 import java.text.ParseException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-
 import com.vitor.cursomc.services.DBService;
+import com.vitor.cursomc.services.EmailService;
+import com.vitor.cursomc.services.MockEmailService;
+
 
 @Configuration
 @Profile("test")
@@ -20,5 +21,10 @@ public class TestConfig {
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new MockEmailService();
 	}
 }
